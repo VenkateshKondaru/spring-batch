184d54d29145e9ea35709bd3be4dcfc6027917cb
Venkatesh & Kondaru <venkateshkondaru1206@gmail.com>
2022-03-24 21:35:22 +0530

    Structure modification in remote

---
+++ src/main/java/com/venkate/boot/batch/entity/Product.java
@@ -0,0 +1,33 @@
+package com.venkate.boot.batch.entity;
+
+public class Product {
+
+	private Integer id;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	private String name;
+	private String description;
+	private Double price;
+}
